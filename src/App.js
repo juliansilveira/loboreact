1e06e382df78e03c9ff4d7a1a07ee33b2065e8aa
Julián Silveira Annoni <87427372+juliansilveira@users.noreply.github.com>
2024-05-10 18:07:20 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
-import Contenido from './components/contenido';
-import Router from 'react'
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Contenido from "./components/contenido";
 // import Desplegable from './components/desplegable'
 
 function App() {
-  return (<>
-  <Header />
-  <Contenido />
-  <Footer />
-
-  </>
+  return (
+    <>
+      <Header />
+      <Contenido />
+      <Footer />
+    </>
   );
 }
 
